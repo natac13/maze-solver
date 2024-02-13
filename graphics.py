8abaf2847fbcb42ce2a6c08acce4433da6b753c7
Sean Campbell <sean.campbell13@gmail.com>
2024-02-13 17:52:15 -0500

    feat: maze class

--- graphics.py
+++ graphics.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
@@ -71,3 +72,56 @@ class Cell:
       self._win.draw_line(Line(Point(x1, y1), Point(x2, y1)), "black")
     if self.has_bottom_wall:
       self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)), "black")
+  
+  def draw_move(self, to_cell, undo=False):
+    ### draw a path between self and to_cell. The line will go from the center of self to the center of to_cell. if undo is True, the line is colored gray, if False, the line is colored red.
+    x1 = (self._x1 + self._x2) // 2
+    y1 = (self._y1 + self._y2) // 2
+    x2 = (to_cell._x1 + to_cell._x2) // 2
+    y2 = (to_cell._y1 + to_cell._y2) // 2
+    if undo:
+      self._win.draw_line(Line(Point(x1, y1), Point(x2, y2)), "gray")
+    else:
+      self._win.draw_line(Line(Point(x1, y1), Point(x2, y2)), "red")
+
+class Maze:
+  def __init__(
+      self,
+      x1,
+      y1,
+      num_rows,
+      num_cols,
+      cell_size_x,
+      cell_size_y,
+      window
+  ):
+    self._x1 = x1
+    self._y1 = y1
+    self._num_rows = num_rows
+    self._num_cols = num_cols
+    self._cell_size_x = cell_size_x
+    self._cell_size_y = cell_size_y
+    self._win = window
+    self._cells = []
+    self._create_cells()
+
+  def _create_cells(self):
+    for i in range(self._num_rows):
+      for j in range(self._num_cols):
+        self._draw_cell(i, j)
+  
+  def _draw_cell(self, i, j):
+    cell = Cell(self._win)
+    cell.draw(
+      self._x1 + i * self._cell_size_x,
+      self._y1 + j * self._cell_size_y,
+      self._x1 + (i + 1) * self._cell_size_x,
+      self._y1 + (j + 1) * self._cell_size_y
+    )
+    self._cells.append(cell)
+    self._animate()
+
+  def _animate(self):
+    self._win.redraw()
+    time.sleep(0.05)
+
--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from graphics import Cell, Line, Point, Window
+from graphics import Cell, Line, Maze, Point, Window
 
 def main():
   window = Window(800, 600)
   # Draw the maze here
 
-  cell1 = Cell(window)
-  cell1.draw(0, 0, 100, 100)
-
-  cell2 = Cell(window)
-  cell2.draw(100, 0, 200, 100)
-
-  cell3 = Cell(window)
-  cell3.draw(200, 0, 300, 100)
-
-  cell4 = Cell(window)
-  cell4.draw(300, 0, 400, 100)
+  maze = Maze(
+    0, 0, 10, 10, 20, 20, window
+  )
 
 
   window.wait_for_close()
